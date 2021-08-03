e9b395306ad5be74b4b1a44e01368b13580c7469
asdf <asdf>
2021-08-03 20:34:21 -0300

    fix

--- src/resources/assets/js/stores/index.js
+++ src/resources/assets/js/stores/index.js
@@ -200,7 +200,7 @@ const store = new Vuex.Store({
               });
             });
             commit('STATUS_REQUEST');
-            // window.location.reload();
+            window.location.reload();
             console.log("Data request: ", data);
             console.log("Orders: ", this.state.orders);
           } else {
