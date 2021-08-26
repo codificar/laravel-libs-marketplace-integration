3e7e2093d2781b1b20901f53696a3596d7533dd9
Eliza <eliza.rocha@codificar.com.br>
2021-08-26 14:50:23 -0300

    fix

--- src/Database/Migrations/2021_06_15_181061_run_add_information_url_key.php
+++ src/Database/Migrations/2021_06_15_181061_run_add_information_url_key.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RunAddIfoodKeysSeeder extends Migration
+class RunAddInformationUrlKey extends Migration
 {
     /**
      * Run the migrations.
