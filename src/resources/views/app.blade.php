ee5a3a90c47c0c60076c4289f8230c89bb3c5227
Raphael Canguçu <raphael.cangucu@codificar.com.br>
2022-06-24 14:35:12 -0300

    change names

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -25,5 +25,5 @@
     <div id="marketplace-integration"></div>
     <script src="/marketplace-integration/lang.trans/settings"> </script> 
     <script src="/marketplace-integration/js/env.js"> </script> 
-    <script src="{{ asset('vendor/codificar/marketplace-integration/js/app.js') }}"></script>
+    <script src="{{ asset('vendor/codificar/marketplace-integration/js/marketplace-integration.js') }}"></script>
 @endsection
