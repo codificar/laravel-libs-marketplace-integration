4528c80bc7b4d5bd8a32d733bcb488d0c9d16196
Raphael Canguçu <raphael.cangucu@codificar.com.br>
2022-06-27 22:07:07 -0300

    Melhorias e ajustes de código

--- src/Http/Controllers/MarketplaceController.php
+++ src/Http/Controllers/MarketplaceController.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 use Codificar\MarketplaceIntegration\Repositories\MarketplaceRepository;
 use Codificar\MarketplaceIntegration\Http\Resources\OrdersResource;
 
+use Codificar\MarketplaceIntegration\Http\Requests\StoreMarketConfigFormRequest;
+
 
 class MarketplaceController extends Controller
 {
@@ -28,22 +30,83 @@ class MarketplaceController extends Controller
     }
     
 
-     /**
+    /**
      * Function to return orders from database
      * @return OrdersResource
      */
-     public function getOrders(Request $request, $shopId = NULL)
-     {
-         
-         $startTime = $request['range'][0] != null ? $request['range'][0] : \Carbon\Carbon::now()->subDays(1);
- 
-         $endTime = $request['range'][0] != null ? $request['range'][0] : null;
+    public function getOrders(Request $request, $shopId = NULL)
+    {
+        
+        $startTime = $request['range'][0] != null ? $request['range'][0] : \Carbon\Carbon::now()->subDays(1);
 
-         $marketId = $request['marketId'] ;
+        $endTime = $request['range'][0] != null ? $request['range'][0] : null;
 
-         $orders =  MarketplaceRepository::getOrders($shopId, $marketId, $startTime, $endTime);
- 
-         return new OrdersResource($orders);
-     }
+        $marketId = $request['marketId'] ;
+
+        $orders =  MarketplaceRepository::getOrders($shopId, $marketId, $startTime, $endTime);
+
+        return new OrdersResource($orders);
+    }
+
+    /**
+     * Function to store (save or update) market config
+     * @return ?
+     */
+    public function storeMarketConfig(StoreMarketConfigFormRequest $request)
+    {
+
+        \DB::beginTransaction();
+
+
+        $factory = MarketplaceFactory::create($request->select);
+        $response = $factory->merchantDetails($marketConfig->merchant_id);
+
+        $latitude   = null;
+        $longitude  = null;
+        $address    = null;
+
+        if($response) {
+            $latitude = $response->address->latitude ;
+            $longitude = $response->address->longitude ;
+            $address = json_encode($response->address) ;
+        }
+
+        $address = MarketConfig::updateOrCreate([
+            'id'            => $request->id
+        ],[
+            'shop_id'       => $request->shop_id,
+            'merchant_id'   => $request->merchant_id,
+            'name'          => $request->merchant_name,
+            'market'        => $request->select ,
+            'latitude'      => $latitude,
+            'longitude'     => $longitude,
+            'address'       => $address,
+        ]);
+
+        \DB::commit();
+
+        return $marketConfig;
+    }
+
+
+    /**
+     * Function to delete market config
+     * @return 
+     */
+    public function deleteMarketConfig(Request $request)
+    {
+        
+        $response = ['success' => false];
+
+        $data = MarketConfig::find($request->id);
+
+        if (is_object($data))
+        {
+            $data->delete();
+            $response['success'] = true;
+        }
+
+        return $response;
+    }
  
 }
