4ebb6b93e77aea3b3459673ddf7c34be614e14db
Raphael Canguçu <raphael.cangucu@codificar.com.br>
2022-07-06 10:02:49 -0300

    hubster credential

--- src/resources/assets/js/pages/Credentials.vue
+++ src/resources/assets/js/pages/Credentials.vue
@@ -82,6 +82,45 @@
               </div>
           </div>
       </div>
+      <div class="card card-outline-info">
+          <div class="card-header">
+              <h4 class="text-white m-b-0">{{ trans('settings.hubster_credentials')}}</h4>
+          </div> 
+
+          <div class="card-block">
+              <div class="row">
+                  <div class="col-md-6 col-sm-12">
+                      <b-form-group
+                          id="input-group-1"
+                          :label="trans('settings.hubster_client_id')"
+                          label-for="hubster_client_id"
+                      >
+                          <b-form-input
+                          id="hubster_client_id"
+                          v-model="settings.hubster_client_id"
+                          type="text"
+                          required
+                          ></b-form-input>
+                      </b-form-group>
+                  </div>
+                  <div class="col-md-6 col-sm-12">
+                      <b-form-group
+                          id="input-group-2"
+                          :label="trans('settings.hubster_client_secret')"
+                          label-for="hubster_client_secret"
+                      >
+                          <b-form-input
+                          id="hubster_client_secret"
+                          v-model="settings.hubster_client_secret"
+                          type="text"
+                          required
+                          ></b-form-input>
+                      </b-form-group>
+                  </div>
+              </div>
+          </div>
+      </div>
+      
       <div class="box-footer">
         <div class="pull-right">
           <div class="row">
@@ -114,6 +153,8 @@ export default {
               dispatch_max_delivery : 3 ,
               ifood_client_id : null ,
               ifood_client_secret : null ,
+              hubster_client_id : null ,
+              hubster_client_secret : null ,
             }
         }
     },
