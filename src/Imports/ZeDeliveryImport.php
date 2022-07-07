5fdc724c42e77c6a2c2d0d2dad645998e714f782
Raphael Canguçu <raphael.cangucu@codificar.com.br>
2022-07-06 22:58:30 -0300

    ride fixes

--- src/Imports/ZeDeliveryImport.php
+++ src/Imports/ZeDeliveryImport.php
@@ -16,9 +16,13 @@ use Codificar\MarketplaceIntegration\Repositories\DispatchRepository ;
 use Codificar\MarketplaceIntegration\Models\MarketConfig;
 use Codificar\MarketplaceIntegration\Models\Shops;
 use Codificar\MarketplaceIntegration\Models\OrderDetails;
+use Codificar\MarketplaceIntegration\Models\DeliveryAddress;
 
 
 use App\Models\RequestPoint;
+
+use Carbon\Carbon;
+
 
 
 use App\Services\EstimateService;
@@ -33,6 +37,15 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
         foreach ($rows as $row) 
         {
             // after group lets get just one
+            $providerKey    = $row[0]['deliveryman_email'];
+            $providerKey    = 'raphael@codificar.com.br';
+            $provider = MarketplaceRepository::getProviderByKey($providerKey);
+            
+            if(!$provider) {
+                \Log::error(sprintf('Por favor cadastre um entregador com a chave %s', $providerKey));
+                continue;
+            }
+
             $ordersArray = [];
             foreach($row as $groupedRow){
                 $storeId        = $groupedRow['poc_id'];
@@ -41,6 +54,8 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
                 $providerKey    = $groupedRow['deliveryman_email'];
                 $customerId     = $orderId ;
                 $customerName   = $orderId ;
+
+                
 
                 $marketConfig = MarketConfig::where('merchant_id', $storeId)->where('market', MarketplaceFactory::ZEDELIVERY)->first();
 
@@ -103,7 +118,7 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
             }
             
             // create a ride from orders array
-            self::createRide($ordersArray, MarketplaceRepository::getProviderByKey($providerKey));
+            self::createRide($ordersArray, $provider);
            
         }
     }
@@ -121,19 +136,23 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
      *
      * @return array
      */
-    public static function createRide(array $shopOrderArray, $provider){
+    public static function createRide(array $shopOrderArray, \Provider $provider){
 
         // ja criada a corrida
         if($shopOrderArray[0]->request_id)
             return ;        
         
+        $ride = new \Requests ;
+        $shop = $shopOrderArray[0]->shop ;
 
         // locationId
         $locationId = null ;
 
+        //\Log::debug(print_r($shopOrderArray[0],1));
+
         //estimativa
-        $ride->type_id = DispatchRepository::getProviderType($shopOrderArray[0]->institution_id);
-        $estimate = EstimateService::estimateProcessPriceTable(0, 0, count($shopOrderArray), $ride->type_id, null, $locationId, $shopOrderArray[0]->institution_id, null, false, null, null, null, null, null, null);
+        $ride->type_id = DispatchRepository::getProviderType($shop->institution_id);
+        $estimate = EstimateService::estimateProcessPriceTable(count($shopOrderArray) * 4, count($shopOrderArray) * 2.5, count($shopOrderArray), $ride->type_id, null, $locationId, $shop->institution_id, null, false, null, null, null, null, null, null);
         $ride->estimate_provider_value = is_array($estimate) && array_key_exists('provider_value', $estimate) ? $estimate['provider_value'] : $estimate->estimate_provider_value;
         $ride->estimate_price = is_array($estimate) && array_key_exists('estimated_price', $estimate)  ? $estimate['estimated_price'] : $estimate->estimated_price;
         $ride->estimate_distance = is_array($estimate) && array_key_exists('distance', $estimate)  ? $estimate['distance'] : $estimate->distance;
@@ -141,30 +160,53 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
         $ride->price_id = is_array($estimate) && array_key_exists('price_id', $estimate) ? $estimate['price_id'] : null;
 
         //demais dados da corrida
-        $ride->user_id = $shopOrderArray[0]->default_user_id;
-        $ride->payment_mode = DispatchRepository::getPaymentMode($shopOrderArray[0]->institution_id);
+        $ride->user_id = $shop->institution->default_user_id;
+        $ride->payment_mode = DispatchRepository::getPaymentMode($shop->institution_id);
         $ride->payment_id =  null;
-        $ride->time_zone = null;
-        $ride->src_address = $shopOrderArray[0]->shop->full_address;
-        $ride->dest_address = $shopOrderArray[0]->shop->full_address;
-        $ride->request_start_time = null;
-        $ride->latitude = $shopOrderArray[0]->shop->latitude;
-        $ride->longitude = $shopOrderArray[0]->shop->longitude;
-        $ride->D_latitude = $shopOrderArray[0]->shop->latitude;
-        $ride->D_longitude = $shopOrderArray[0]->shop->longitude;
+        $ride->time_zone = env('TIMEZONE', 'UTC');
+        $ride->src_address = $shop->full_address;
+        $ride->dest_address = $shop->full_address;
+        $ride->request_start_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->provider_acceptance_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->provider_started_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->request_start_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->provider_arrived_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->request_finish_time = $shopOrderArray[0]->created_at_marketplace;
+        $ride->latitude = $shop->latitude;
+        $ride->longitude = $shop->longitude;
+        $ride->D_latitude = $shop->latitude;
+        $ride->D_longitude = $shop->longitude;
         $ride->req_create_user_time = time();
-        $ride->current_provider = $provider->id ; // carrega o prestador mais proximo do local de acordo com o tipo/categoria e que não esteja recebendo chamada
+        $ride->current_provider = $provider->id ;
+        $ride->confirmed_provider = $provider->id ;
         $ride->location_id = $locationId;
-        $ride->institution_id = $shopOrderArray[0]->shop->institution_id;
-        $ride->emergency_contact_user = null;
+        $ride->institution_id = $shop->institution_id;
+        $ride->emergency_contact_user = 0;
         $ride->return_to_start = true;
-        
         $ride->category_id = null;
 		$ride->costcentre_id = null;
         $ride->is_automation = false;
+        $ride->is_completed = true;
+        $ride->is_cancelled = false;
+        $ride->own_motoboy  = true;
         
         //salva a corrida
         $ride->save();
+
+        //salva o meta
+        $requestMeta =  new \RequestMeta ;
+        $requestMeta->request_id = $ride->id;
+		$requestMeta->provider_id = $provider->id;
+		$requestMeta->resend_id = $ride->resend_id;
+		$requestMeta->number_resend = 1; //primeiro disparo
+        $requestMeta->save();
+
+        // associa o servico
+        $requestService =  new \RequestServices;
+        $requestService->request_id = $ride->id;
+		$requestService->type = $ride->type_id ;
+		$requestService->category_id = null;
+		$requestService->save();
 
 
         // set payments for user
@@ -173,45 +215,37 @@ class ZeDeliveryImport implements ToCollection, WithChunkReading, ShouldQueue, W
         // salvar pontos para estatisticas depois
 
         $letter = 1 ;
-        //primeiro ponto / orige da loja
-        $point['title']                         =  chr(64 + $letter);
-        $point['action']                        = trans('requests.take_package');
-        $point['action_type']                   = RequestPoint::action_take_package;
-        $point['collect_value']                 = null ;
-        $point['change']                        = null ;
-        $point['form_of_receipt']               = null ;
-        $point['collect_pictures']              = false;
-        $point['collect_signature']             = false ;
-        $point['latitude']                      = $shopOrderArray[0]->shop->latitude;
-        $point['longitude']                     = $shopOrderArray[0]->shop->longitude;
-        $point['address']                       = $shopOrderArray[0]->shop->full_address;
-        $point['order_id']                      = null;
-
-        //novo ponto
-        $requestPoint = new RequestPoint();
-        $requestPoint->store($point, $key - 1, $estimate);
+        //primeiro ponto / origem da loja
+        $requestPoint               = new RequestPoint();
+        $requestPoint->title        =  chr(64 + $letter);
+        $requestPoint->action_type  = RequestPoint::action_collect_order;
+        $requestPoint->action       = trans('marketplace-integration::zedelivery.action_collect_order');
+        $requestPoint->latitude     = $shop->latitude;
+        $requestPoint->longitude    = $shop->longitude;
+        $requestPoint->address      = $shop->full_address;
+        $requestPoint->request_id   = $ride->id;
+        $requestPoint->start_time   = $shopOrderArray[0]->created_at_marketplace;
+        $requestPoint->arrival_time = $shopOrderArray[0]->created_at_marketplace;
+        $requestPoint->finish_time  = $shopOrderArray[0]->created_at_marketplace;
+        $requestPoint->save();
         $letter += 1;
 
 
         foreach ($shopOrderArray as $key => $order) {
 
-            //primeiro ponto / orige da loja
-            $point['title']                         =  chr(64 + $letter);
-            $point['action']                        = trans('requests.take_package');
-            $point['action_type']                   = RequestPoint::action_take_package;
-            $point['collect_value']                 = null ;
-            $point['change']                        = null ;
-            $point['form_of_receipt']               = null ;
-            $point['collect_pictures']              = false;
-            $point['collect_signature']             = false ;
-            $point['latitude']                      = $shopOrderArray[0]->shop->latitude;
-            $point['longitude']                     = $shopOrderArray[0]->shop->longitude;
-            $point['address']                       = $shopOrderArray[0]->shop->full_address;
-            $point['order_id']                      = null;
-
             //novo ponto
-            $requestPoint = new RequestPoint();
-            $requestPoint->store($point, $key - 1, $estimate);
+            $requestPoint               = new RequestPoint();
+            $requestPoint->title        =  chr(64 + $letter);
+            $requestPoint->action_type  = RequestPoint::action_delivery_order;
+            $requestPoint->action       = trans('marketplace-integration::zedelivery.action_delivery_order', ['orderId' => $order->order_id]);
+            $requestPoint->latitude     = $shop->latitude;
+            $requestPoint->longitude    = $shop->longitude;
+            $requestPoint->address      = $shop->full_address;
+            $requestPoint->request_id   = $ride->id;
+            $requestPoint->start_time   = $order->created_at_marketplace;
+            $requestPoint->arrival_time = $order->created_at_marketplace;
+            $requestPoint->finish_time  = $order->created_at_marketplace;
+            $requestPoint->save();
             $letter += 1;
 
             $order->request_id  = $ride->id ;
